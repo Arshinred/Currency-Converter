498eb1cb02fa202e1fa77bdfa5559faf0e8888b9
Arshad H <133554468+Arshinred@users.noreply.github.com>
2024-01-21 23:35:30 -0500

    Update CurrencyConverter.cpp
    Added another currency

--- CurrencyConverter.cpp
+++ CurrencyConverter.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 int main(){
 
-    cout << "----------ALL CONVERSION RATES ARE FROM 1/16/24----------" << endl;
+    cout << "-------------------ALL CONVERSION RATES ARE FROM JANUARY 14-16 OF 2024-------------------" << endl;
     cout << "1. US Dollar (USD) \n" <<  "2. Euro (EUR) \n" << "3. Japanese Yen (JPY)" << endl; 
     cout << "4. Pount sterling (GBP) \n" <<  "5. Australian dollar (AUD) \n" << "6. Canadian dollar (CAD)" << endl; 
-    cout << "7. Bangladeshi Taka (BDT)"<< endl;
-    cout << "Select your currency by inputting 1-7 :";
+    cout << "7. Bangladeshi Taka (BDT) \n" << "8. Saudi Riyal (SR)" <<  endl; 
+    cout << "-----------------------------------------------------------------------------------------" << endl;
+    cout << "Select your currency by inputting 1-8 :";
 
-
-    int currencyType; // Inputting currency type to convert FROM
+    int currencyType;           // Inputting currency type to convert FROM
     cin >> currencyType;
     string FromCurrency;
-    switch(currencyType) {  // Displays selection to user
+    switch(currencyType) {      // Displays selection to user
         case 1:
         cout << "Currency chosen is USD" << endl;
         FromCurrency = "USD";
@@ -43,6 +43,10 @@ int main(){
         cout << "Currency chosen is BDT" << endl;
         FromCurrency = "BDT";
         break;
+        case 8:
+        cout << "Currency chosen is SR" << endl;
+        FromCurrency = "SR";
+        break;
         default:
         cout << "Error" << endl;
         exit(0);
@@ -56,14 +60,14 @@ int main(){
  
     cout << "1. US Dollar (USD) \n" <<  "2. Euro (EUR) \n" << "3. Japanese Yen (JPY)" << endl; 
     cout << "4. Pount sterling (GBP) \n" <<  "5. Australian dollar (AUD) \n" << "6. Canadian dollar (CAD)" << endl;
-    cout << "7. Bangladeshi Taka (BDT)"<< endl;
-    cout << "Select currency to convert to by inputting 1-7: ";
+    cout << "7. Bangladeshi Taka (BDT)" << "8. Saudi Riyal (SR)" << endl; 
+    cout << "Select currency to convert to by inputting 1-8: ";
     
-    int conversionType; // Inputting currency to convert TO
+    int conversionType;         // Inputting currency to convert TO
     cin >> conversionType;
     string ToCurrency;
     
-    switch(conversionType) {  // Displays selection to user
+    switch(conversionType) {    // Displays selection to user
         case 1:
         cout << "Currency converting to is USD" << endl; 
         ToCurrency = "USD";
@@ -91,27 +95,32 @@ int main(){
         cout << "Currency converting to is BDT" << endl;
         ToCurrency = "BDT";
         break;
+        case 8:
+        cout << "Currency converting to is SR" << endl;
+        ToCurrency = "SR";
+        break;
         default:
         cout << "Error re-run program and input allowed values" << endl;
         exit(0);
         break;
     }
 
-    if(currencyType == conversionType) { // stops code if same currency is picked twice
-        cout << "Cannot convert, selected currencies are the same" << endl;
+    if(currencyType == conversionType) {        // stops code if same currency is picked twice
+        cout << "Conversion not needed, selected currencies are the same" << endl;
         exit(0);
     }
 
     // Conversion rates from 1/16/2024 according to google
 
-    double conversionRates[7][7] = {    // Conversion rates of each currency to itself and other currencies in the list
-        {1.0, 0.91, 145.11, 0.79, 1.50, 1.34, 109.91}, // USD 
-        {1.10, 1.0, 158.90, 0.86, 1.64, 1.47, 119.56}, // EUR
-        {0.0069, 0.0063, 1.0, 0.0054, 0.010, 0.0092, 0.75}, // JPY
-        {1.27, 1.16, 184.83, 1.0, 1.90, 1.71, 138.93}, // GBP
-        {0.67, 0.61, 97.05, 0.53, 1.0, 0.90, 72.40}, // AUD
-        {0.75, 0.68, 108.25, 0.59, 1.12, 1.0, 81.49}, // CAD
-        {0.0091, 0.0084, 1.34, 0.0072, 0.014, 0.012, 1.0} //BDT    // Rates from 1/16/24
+    double conversionRates[8][8] = {    // Conversion rates of each currency to itself and other currencies in the list
+        {1.0, 0.91, 145.11, 0.79, 1.50, 1.34, 109.91, 3.75},        // USD 
+        {1.10, 1.0, 158.90, 0.86, 1.64, 1.47, 119.56, 4.08 },       // EUR
+        {0.0069, 0.0063, 1.0, 0.0054, 0.010, 0.0092, 0.75, 0.025},  // JPY
+        {1.27, 1.16, 184.83, 1.0, 1.90, 1.71, 138.93, 4.74},        // GBP
+        {0.67, 0.61, 97.05, 0.53, 1.0, 0.90, 72.40, 2.47},          // AUD
+        {0.75, 0.68, 108.25, 0.59, 1.12, 1.0, 81.49, 2.78},         // CAD
+        {0.0091, 0.0084, 1.34, 0.0072, 0.014, 0.012, 1.0, 0.034},   //BDT    
+        {0.27, 0.25, 39.28, 0.21, 0.40, 0.36, 29.31, 1.0 }          //SR 
     };
 
     // Perform the conversion calculations
